82b4aa55fea5f140d81e393ae7f3a8b36308dcbe
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2024-10-18 15:31:48 +0700

    Mengimplementasikan layout Kolom dengan padding, penyelarasan vertikal dan horizontal.

--- app/src/main/java/com/example/mainactivity/MainActivity.kt
+++ app/src/main/java/com/example/mainactivity/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mainactivity.ui.theme.MainActivityTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,11 +41,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(6.dp),
+        verticalArrangement = Arrangement.spacedBy(2.dp),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            text = "Login", fontSize = 30.sp, fontWeight = FontWeight.Bold,
+            style = androidx.compose.ui.text.TextStyle(
+                shadow = Shadow(
+                    color = Color.DarkGray,
+                    offset = Offset(4f, 4f),
+                    blurRadius = 8f
+                )
+            )
+        )
+
+
 
 @Preview(showBackground = true)
 @Composable
